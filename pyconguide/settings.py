ec3374237ddc3bbbff52a13435f3d79896b94d3d
Jeremy Carbaugh <jeremy@isl.co>
2017-04-11 17:52:45 -0400

    upgrade requirements

--- pyconguide/settings.py
+++ pyconguide/settings.py
@@ -52,7 +52,6 @@ if DEBUG and DEBUG_TOOLBAR:
 INSTALLED_APPS += ('django_rq',)
 
 MIDDLEWARE_CLASSES = (
-    'sslify.middleware.SSLifyMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
